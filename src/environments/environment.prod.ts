eda1cf279bf41d650e29dbe1f314170e27ad0338
Victor <victorsmirnov@me.com>
2022-10-11 09:57:35 +0300

    production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  databaseUrl: 'https://angular-e-commerce-vs-default-rtdb.europe-west1.firebasedatabase.app',
+  storageBucket: 'gs://angular-e-commerce-vs.appspot.com',
+  firebase: {
+    apiKey: "AIzaSyACj_E4CtunepLUO84WxNpGiOGncmLacik",
+    authDomain: "angular-e-commerce-vs.firebaseapp.com",
+    databaseURL: "https://angular-e-commerce-vs-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "angular-e-commerce-vs",
+    storageBucket: "angular-e-commerce-vs.appspot.com",
+    messagingSenderId: "259308924205",
+    appId: "1:259308924205:web:cbbb28c4602fde0eb877a4",
+  },
 };
